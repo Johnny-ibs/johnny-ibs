5214edd0f3085c435c7c072915d3b69b3f221b52
Johnny-ibs <151755689+Johnny-ibs@users.noreply.github.com>
2023-11-22 15:30:44 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,29 @@
+input.onGesture(Gesture.Shake, function () {
+    let Spielstand = 0
+    game.setScore(randint(1, 3))
+    if (Spielstand == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (Spielstand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    }
+})
